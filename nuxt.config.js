cbcf9069f9a210ac0707c2f3d27d39e03d081e33
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-03-30 21:43:46 +0900

    feat: fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   css: ['~/assets/neomo/src/neomo.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/fontawesome.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -51,4 +51,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-};
+}
