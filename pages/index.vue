56e39be13e3a6cd0043dd6aaf7fb0005fc6618cd
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-03-31 02:15:05 +0900

    refactoring: index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,30 +12,26 @@
       </div>
       <!-- Logo End-->
 
-      <div style="margin-bottom: 50px">
-        <span class="font" style="width: 100vw">WELCOME TO</span>
+      <div class="content">
+        <span class="font">WELCOME TO</span>
         <p class="font-name">NEOMO</p>
-        <div style="margin-top: 30px">
+        <div class="mt-10">
           <p class="font-content">
             Neomo is a reactive framework designed to help developers develop
             web and app designs with neomorphism.
           </p>
-          <p class="font-content" style="margin-top: 30px">
-            Try using it right now.
-          </p>
+          <p class="font-content mt-5">Try using it right now.</p>
         </div>
       </div>
 
       <!-- Button Start -->
       <div>
-        <button class="button outset-neomo index-btn" to="/table">
-          <span class="ma-0 button-font">GET STARTED</span>
+        <button class="button outset-neomo content-btn--outset" to="/table">
+          <span class="ma-0 content-btn--font">GET STARTED</span>
         </button>
-        <button class="button outset-neomo index-btn">
-          <span class="ma-0 button-font">DOWNLOAD</span>
-          <span style="margin: 7px 0 0 5px; font-size: 0.7rem; color: grey">
-            v0.0.1
-          </span>
+        <button class="button outset-neomo content-btn--outset">
+          <span class="ma-0 content-btn--font">DOWNLOAD</span>
+          <span class="content-btn--version"> v0.0.1 </span>
         </button>
       </div>
       <!-- Button End -->
@@ -56,33 +52,36 @@ export default {}
   background-color: #eff8ff;
   padding: 80px 0 80px 0;
 }
-/* body Custom CSS End */
+.content {
+  margin-bottom: 50px;
+}
 
 /* font Custom CSS Start */
 .font {
   font-size: 2.5rem;
   color: rgb(117, 117, 117);
 }
-
 .font-name {
   font-weight: 900;
   font-size: 6rem;
   margin-top: 5px;
   margin-bottom: 0;
 }
-
 .font-content {
   font-size: 1.2rem;
   color: black;
 }
-/* font Custom CSS End */
 
 /* Button Custom CSS Start */
-.button-font {
+.content-btn--font {
   font-size: 1.3rem;
 }
-
-.index-btn {
+.content-btn--version {
+  margin: 7px 0 0 5px;
+  font-size: 0.7rem;
+  color: grey;
+}
+.content-btn--outset {
   width: 250px;
   min-height: 70px;
   margin: 10px 50px 0 50px;
@@ -90,19 +89,16 @@ export default {}
   border-radius: 100px;
   border: none;
 }
-
-.index-btn:focus {
+.content-btn--outset:focus {
   outline: 0;
 }
-
-.index-btn:hover {
+.content-btn--outset:hover {
   border: none;
   background: var(--neomo);
   box-shadow: inset 5px 5px 9px #cad3da, inset -5px -5px 9px #ebf1f5 !important;
 }
-/* Button Custom CSS End */
 
-#content-layout {
+#main-layout {
   margin: 56px 0 0 0 !important;
 }
 </style>
