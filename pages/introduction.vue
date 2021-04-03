0dd7461ba669006d08dce846b6ecfa51a33cd0db
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-04-03 17:07:49 +0900

    add: introduction.vue

---
+++ pages/introduction.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Introduction</h1>
+    <client-only>
+      <p>
+        Get started with NEOMO, the framework designed for neomorphism, use the
+        Welcome page and a variety of example codes.
+      </p>
+    </client-only>
+
+    <h2 style="margin-top: 100px">What is NEOMO?</h2>
+    <client-only>
+      <p>
+        NEOMO is a UI framework designed with neomorphism. To use neomorphism,
+        which has emerged as a new design trend, developers have the
+        inconvenience of going through customizing themselves. To reduce the
+        inconvenience above, NEOMO provides UI designed with neomorphism. NEOMO
+        is developed with the goal of providing various example codes.
+      </p>
+    </client-only>
+  </div>
+</template>
+
+<style scoped></style>
