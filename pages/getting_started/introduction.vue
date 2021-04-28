bf761d14c61cbb2127310e6d76948f42f83dcfd3
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-04-28 22:31:07 +0900

    add: introduction.vue

---
+++ pages/getting_started/introduction.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <h1>Introduction</h1>
+    <client-only>
+      <p class="bottom-border">
+        Get started with NEOMO, a
+        <strong class="text-strong">UI framework</strong> for
+        <strong class="text-strong">neomorphism</strong> design. <br /><a
+          href="/"
+          >v1.0.0 Docs</a
+        >
+        kindly provide you with the usage and example code.
+      </p>
+    </client-only>
+
+    <h2 id="What-is-NEOMO?">What is NEOMO?</h2>
+    <client-only>
+      <p>
+        NEOMO is a <strong class="text-strong">UI framework</strong> designed
+        with <strong class="text-strong">neomorphism</strong>. To use
+        neomorphism, which has emerged as a new design trend, developers have
+        the <strong class="text-strong">inconvenience</strong> of going through
+        <strong class="text-strong">customizing</strong> themselves.<br />
+      </p>
+      <p>
+        <strong class="text-strong">To reduce</strong> the inconvenience above,
+        NEOMO provides UI designed with neomorphism. NEOMO is developed with the
+        <strong class="text-strong">goal</strong> of providing various example
+        codes.
+      </p>
+    </client-only>
+
+    <h2 id="License">License</h2>
+    <client-only>
+      <p>
+        NEOMO's license is <strong class="text-strong">MIT license</strong>. You
+        can check it out on
+        <a href="https://github.com/neomorphism/neomo/blob/main/LICENSE"
+          >github</a
+        >
+        .
+      </p>
+    </client-only>
+    <h2 id="Contribute">Contribute</h2>
+    <client-only>
+      <p>
+        You can be a <strong class="text-strong">developer</strong> at the same
+        time as a NEOMO user. <br />Use a variety of methods to
+        <strong class="text-strong">contribute</strong> to NEOMO.
+      </p></client-only
+    >
+    <ul>
+      <li>
+        <a href="https://github.com/neomorphism/neomo/blob/main/LICENSE"
+          >Github</a
+        >
+      </li>
+      <li>
+        <a href="https://github.com/neomorphism/neomo/blob/main/LICENSE"
+          >Instagram</a
+        >
+      </li>
+      <li>
+        <a href="https://github.com/neomorphism/neomo/blob/main/LICENSE"
+          >Facebook</a
+        >
+      </li>
+      <li>
+        <a href="https://github.com/neomorphism/neomo/blob/main/LICENSE"
+          >Email</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+a {
+  color: rgb(32, 133, 216);
+}
+</style>
