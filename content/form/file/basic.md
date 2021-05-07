884961b5bc5fe29c6603beaaa5ec57fac7c13674
damin <dnflekals@ajou.ac.kr>
2021-05-07 17:07:25 +0900

    add: file (vue, md)

---
+++ content/form/file/basic.md
@@ -0,0 +1,7 @@
+```html
+<div class="file">
+  <input class="file-name" value="Choose a file..." disabled="disabled" />
+  <label for="file-label">Upload</label>
+  <input id="file-label" type="file" class="file-hidden" />
+</div>
+```
---
+++ pages/form/file.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h1>Checkbox</h1>
+    <client-only>
+      <p class="bottom-border">
+        File class can be useful when you upload a file.
+      </p>
+    </client-only>
+
+    <div class="tab">
+      <ul class="tab-content outset-neomo">
+        <!-- Content-1 Start -->
+        <li id="content-1" class="tab-content--list">
+          <div class="tab-content--title"><h2>Basic</h2></div>
+
+          <!-- Content-1 Basic start -->
+          <div class="card inset-neomo code-exam">
+            <div class="mt-2 mb-3" style="display: flow-root">
+              <button
+                class="button outset-neomo button-exam"
+                onclick="ContentToggle()"
+              >
+                <i class="fas fa-code collapse"></i>
+              </button>
+            </div>
+
+            <div class="mt-2 expanded">
+              <nuxt-content :document="basic" />
+            </div>
+
+            <div style="text-align: center">
+              <div class="file">
+                <input
+                  class="file-name"
+                  value="Choose a file..."
+                  disabled="disabled"
+                />
+                <label for="file-label">Upload</label>
+                <input id="file-label" type="file" class="file-hidden" />
+              </div>
+            </div>
+          </div>
+          <!-- Content-1 Basic end -->
+
+          <!-- Content-1 end -->
+        </li>
+      </ul>
+
+      <!-- Tab List start -->
+      <ul class="tab-list tab-right">
+        <li><a id="tab-1" href="#" class="current">Basic</a></li>
+      </ul>
+      <!-- Tab List end -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const basic = await $content(params.slug || "form/file/basic").fetch();
+
+    return {
+      basic,
+    };
+  },
+};
+</script>
