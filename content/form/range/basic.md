e01b614508dd1903b82787f0c17cde08e3fba760
damin <dnflekals@ajou.ac.kr>
2021-05-07 18:43:29 +0900

    add: range(vue, md, js)

---
+++ content/form/range/basic.md
@@ -0,0 +1,10 @@
+```html
+<input
+  id="myRange"
+  type="range"
+  min="1"
+  max="100"
+  class="range-round inset-neomo"
+/>
+<p>Value: <span id="demo"></span></p>
+```
---
+++ pages/form/range.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1>Range</h1>
+    <client-only>
+      <p class="bottom-border">
+        Radio can also be used as a neomo design. Try using it simply.
+      </p>
+    </client-only>
+
+    <div class="tab">
+      <ul class="tab-content outset-neomo">
+        <!-- Content-1 Start -->
+        <li id="content-1" class="tab-content--list">
+          <div class="tab-content--title"><h2>Basic</h2></div>
+
+          <!-- Content-1 Basic start -->
+          <div class="card inset-neomo code-exam">
+            <div class="mt-2 mb-3" style="display: flow-root">
+              <button
+                class="button outset-neomo button-exam"
+                onclick="ContentToggle()"
+              >
+                <i class="fas fa-code collapse"></i>
+              </button>
+            </div>
+
+            <div class="mt-2 expanded">
+              <nuxt-content :document="basic" />
+            </div>
+
+            <input
+              id="myRange"
+              type="range"
+              min="1"
+              max="100"
+              class="range-round inset-neomo"
+            />
+            <p>Value: <span id="demo"></span></p>
+          </div>
+          <!-- Content-1 Basic end -->
+
+          <!-- Content-1 end -->
+        </li>
+      </ul>
+
+      <!-- Tab List start -->
+      <ul class="tab-list tab-right">
+        <li><a id="tab-1" href="#" class="current">Basic</a></li>
+      </ul>
+      <!-- Tab List end -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const basic = await $content(params.slug || "form/range/basic").fetch();
+
+    return {
+      basic,
+    };
+  },
+};
+</script>
