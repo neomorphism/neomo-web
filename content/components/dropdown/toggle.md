77d4103d34058e6c3103a9e0308b6644452be4a5
damin <dnflekals@ajou.ac.kr>
2021-05-26 22:56:16 +0900

    fix: dropdown (v2.0.0)

--- content/components/dropdown/toggle.md
+++ content/components/dropdown/toggle.md
@@ -1,13 +1,18 @@
 ```html
-<div class="dropdown-toggle">
-  <button class="dropdown-toggle--button outset-neomo">
-    Toggle
-    <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-toggle--content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
+<div style="text-align: center">
+  <div class="dropdown-toggle">
+    <button
+      data-dropdown-id="dropdown"
+      class="dropdown-toggle--button outset-neomo"
+    >
+      Toggle
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div data-dropdown-id="dropdown" class="dropdown-toggle--content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
   </div>
 </div>
 ```
--- pages/components/dropdown.vue
+++ pages/components/dropdown.vue
@@ -117,11 +117,17 @@
               </div>
               <div style="text-align: center">
                 <div class="dropdown-toggle">
-                  <button class="dropdown-toggle--button outset-neomo">
+                  <button
+                    data-dropdown-id="dropdown"
+                    class="dropdown-toggle--button outset-neomo"
+                  >
                     Toggle
                     <i class="fa fa-caret-down"></i>
                   </button>
-                  <div class="dropdown-toggle--content">
+                  <div
+                    data-dropdown-id="dropdown"
+                    class="dropdown-toggle--content"
+                  >
                     <a href="#">Link 1</a>
                     <a href="#">Link 2</a>
                     <a href="#">Link 3</a>
